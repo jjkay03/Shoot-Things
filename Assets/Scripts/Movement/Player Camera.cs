1c5788480f8f08039ff4c7cc7b91d8e68afca0ce
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2024-03-21 13:22:43 +0100

    Created player movement

---
+++ Assets/Scripts/Movement/Player Camera.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour {
+    /* -------------------------------- Variables ------------------------------- */
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
+
+    private void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    /* --------------------------------- Methods -------------------------------- */
+    private void Update() {
+        // Get mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // Rotate cam and orientation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
